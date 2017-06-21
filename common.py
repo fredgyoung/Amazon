a159679132cdda9f8bb42671bc559d0ecd8fb202
Srinivas Rao Narne <c_snarne@quicinc.com>
2017-06-21 13:10:56 -0700

    more code

--- common.py
+++ common.py
@@ -1,30 +1,56 @@
 
 
-def find_common(str1, str2):
+def is_anagram(str1, str2, case_sensitive=True):
 
-    if type(str1) == int:
-        str1 = str(str1)
+    # Check for Nulls
+    if str1 is None or str2 is None:
+        print("Cannot compare Null variables")
+        return
 
-    if type(str2) == int:
-        str2 = str(str2)
+    # If we don't care about case
+    if not case_sensitive:
+        str1 = str1.lower()
+        str2 = str2.lower()
 
-    #print( str1, str2)
-
-    result = ''
-
+    # Convert to list
+    str1 = list(str1)
+    str2 = list(str2)
+    
     for letter in str1:
         if letter in str2:
-            result += letter
+            str2.remove(letter)
+        else:
+            print("Fail")
+            return
 
-    return result
+    if len(str2) > 0:
+        print("Fail")
+    else:
+        print("Pass")
+
+    return
+
+"""
+1. f (string, strong) // happy path
+2. f (null, null) // both null
+3. f ("", "") // both empty strings
+4. f (" ", " ") // both empty strings with spaces - blank strings
+5. f ("abc", "zxy") // no matches
+6. f ("!@#$", "1@#!@") // special chars
+7. f (null, "somestring") // 1st param null other not
+8. f ("somestring", null) // 2nd param null other not
+9. Test with very large inputs
+10. f ("azzzzzzzz", "zzzzzzza") // mathcing in different positions
+"""
 
 
-x = find_common('string', 'strong')
-print(x)
-
-x = find_common(1234, 2345)
-print(x)
-
-x = find_common(['a', 'b'], ['b', 'c'])
-print(x)
+is_anagram('string', 'gnirts')
+is_anagram(None, None)
+is_anagram('', '')
+is_anagram(' ', ' ')
+is_anagram('abc', 'def')
+is_anagram('!@#$', '$#@!')
+is_anagram(None, 'abc')
+is_anagram('abc', None)
+is_anagram('azzzzzzzzz', 'zzzzzzzzza')
 
